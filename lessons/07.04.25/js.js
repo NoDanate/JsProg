6ab87d06534030241d948039086158a78c1be7cb
Fedor <fdor.peshkov@bk.ru>
2025-04-07 20:09:22 +0500

    strings and objects

--- lessons/07.04.25/js.js
+++ lessons/07.04.25/js.js
@@ -1,6 +1,4 @@
-let yourCar = prompt("Какая у вас машина?", "машины нет");
-
-const person ={
+/*const person ={
     name: "Fedor",
     age: 16,
     "likes cats": true,
@@ -9,11 +7,50 @@ const person ={
         zip: "643251",
     },
 
-    [yourCar]: 1,
+    add(a, b){
+        alert( a + b);
+    },
 
     greet(){
         alert(`Привет, я ${this.name}`);
     }
-}
+};*/
 
-alert(yourCar["машины нет"]);
+//const calculateAB = person.add(123, 1);
+
+//calculateAB();
+
+//person.greet();
+
+let cat = new Object();
+cat.breed = "Aby";
+
+let array = new Array();
+let array2 = ["first", "second"];
+let array3 = [1, 2, 3, 4, 5, true, "string"];
+
+//alert(array3.map(n => n*2));
+///alert(array3.filter(n => n % 2 === 0));
+//alert(array3.reduce((sum, n) => sum + n, 0));
+array3[3] = 6;
+
+//array3.forEach((n, i) => alert(i, n));
+
+const dogs = [
+    {id: 1, name: "Jack"},
+    {id: 2, name: "Malysh"}
+];
+
+let names = dogs.map(n => n.name);
+
+//alert(names);
+
+let fruits = ["apples", "banana"];
+
+fruits.push("cherry");
+
+let first = fruits.shift();
+
+alert(fruits);
+
+alert(first);
---
+++ lessons/07.04.25/string.js
@@ -0,0 +1,7 @@
+let s = "hey students                ";
+alert(s.trim());
+alert(s.toUpperCase());
+alert(s.toLowerCase());
+alert(s.includes("students"));
+alert(s.slice(1, 3));
+alert(s.split());
